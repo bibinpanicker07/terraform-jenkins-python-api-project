f86d99a2f0f34eed4c7fa6838b626aa696321e05
bibinpanicker07 <114976103+bibinpanicker07@users.noreply.github.com>
2025-07-09 00:01:35 +0530

    Create main.tf

---
+++ terraform/modules/secret-manager/main.tf
@@ -0,0 +1,18 @@
+resource "random_password" "db_password" {
+  length  = 16
+  special = true
+}
+
+resource "aws_secretsmanager_secret" "rds_secret" {
+  name        = "rds-db-credentials"
+  description = "Credentials for RDS MySQL database"
+}
+
+resource "aws_secretsmanager_secret_version" "rds_secret_version" {
+  secret_id     = aws_secretsmanager_secret.rds_secret.id
+  secret_string = jsonencode({
+    username = "dbuser"
+    password = random_password.db_password.result
+  })
+}
+
