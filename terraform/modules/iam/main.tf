998206b8301ffdd8fab5384f2e0d4629028e4af3
bibinpanicker07 <114976103+bibinpanicker07@users.noreply.github.com>
2025-07-09 00:05:19 +0530

    Create main.tf

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-secretsmanager-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+
+resource "aws_iam_policy" "secretsmanager_access_policy" {
+  name        = "SecretsManagerReadPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = aws_secretsmanager_secret.rds_secret.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.secretsmanager_access_policy.arn
+}
+
+# Create an instance profile to attach to EC2 instances
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
