bd51dc619239a6d1fa7ccb8bf27515b7083c8458
bibinpanicker07 <114976103+bibinpanicker07@users.noreply.github.com>
2025-07-08 23:47:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region         = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "bibinpanicker.shop"
+    key    = "python-api-project/terraform.tfstate"
+    region = "es-east-1"
+  }
+}
 module "networking" {
     source              = "./modules/networking"
     vpc_cidr_block      = var.vpc_cidr_block
